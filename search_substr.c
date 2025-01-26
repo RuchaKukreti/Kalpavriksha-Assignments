183329254915a18d48ae81f8d116b89c7b5c6657
Rucha Kukreti <rucha.kukreti@intimetec.com>
2025-01-26 16:34:48 +0530

    Modularized the code, added checks for allocated memory, improved naming of variables, handled against some edge cases as per review comments

--- search_substr.c
+++ search_substr.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-int found_substring(char **array, int row, char *substring)
+int foundSubstring(char **array, int row, char *substring)
 {
     int iterator = 0, column = 0, flag = 0;
     while (*(*(array + row) + (column)) != '\0')
     {
         if (*(*(array + row) + (column)) == substring[0])
         {
+            iterator = 0;
             while (*(substring + iterator) != '\0')
             {
                 if (*(substring + iterator) == *(*(array + row) + (column)))
@@ -31,31 +32,58 @@ int found_substring(char **array, int row, char *substring)
     }
     return 0;
 }
+int inputStrings(char **array, int totalstrings)
+{
+    for (int iterator = 0; iterator < totalstrings; iterator++)
+    {
+        *(array + iterator) = malloc(100 * sizeof(char));
+        if (*(array + iterator) == NULL)
+            return 1;
+        printf("Enter string: ");
+        scanf("%s", *(array + iterator));
+    }
+    return 0;
+}
+int calculateNumberOfOccurence(char **array, int totalstrings, char *substring)
+{
+    int numberOfOccurence = 0;
+    for (int iterator = 0; iterator < totalstrings; iterator++)
+    {
+        numberOfOccurence += foundSubstring(array, iterator, substring);
+    }
+    return numberOfOccurence;
+}
+void freeArray(char **array, int totalstrings)
+{
+    for (int iterator = 0; iterator < totalstrings; iterator++)
+    {
+        free(array[iterator]);
+    }
+    free(array);
+}
 int main()
 {
     int totalstrings = 0;
     char substring[100] = {'\0'};
     printf("Enter number of strings:");
     scanf("%d", &totalstrings);
-    char **array = malloc(totalstrings * sizeof(char *));
-    printf("Enter strings.\n");
-    for (int row = 0; row < totalstrings; row++)
+    if (totalstrings <= 0)
     {
-        *(array + row) = malloc(100 * sizeof(char));
-        scanf("%s", *(array + row));
+        printf("Enter positive and non-zero value for number of strings.");
+        return 0;
+    }
+    char **array = malloc(totalstrings * sizeof(char *));
+    if (array == NULL || inputStrings(array, totalstrings))
+    {
+        printf("Memory allocation failed.\n");
+        return 0;
     }
     printf("Enter the substring: ");
     scanf("%s", substring);
-    int count = 0;
-    for (int row = 0; row < totalstrings; row++)
-    {
-        count += found_substring(array, row, substring);
-    }
-    printf("Number of occurences:%d", count);
-    for (int row = 0; row < totalstrings; row++)
-    {
-        free(array[row]);
-    }
-    free(array);
+    printf("The substring is found in the following strings:\n");
+    int numberOfOccurence = calculateNumberOfOccurence(array, totalstrings, substring);
+    printf("Number of occurences:%d", numberOfOccurence);
+    freeArray(array, totalstrings);
+    array = NULL;
     return 0;
 }
