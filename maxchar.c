1665cff351166257af517c689fb8b69be3e3c3e1
Rucha Kukreti <rucha.kukreti@intimetec.com>
2025-01-26 14:58:22 +0530

    Modularized the code, added checks for memory allocation, set pointers to NULL after freeing, improved naming of variables, added formatting

--- maxchar.c
+++ maxchar.c
@@ -1,52 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-void copy_string(char *string1, char *string2)
+#define maxLength 50
+int createArrayOfStrings(char **array, int numberOfStrings)
 {
-    int itr = 0;
-    while (string2[itr] != '\0')
+    for (int iterator = 0; iterator < numberOfStrings; iterator++)
     {
-        string1[itr] = string2[itr];
-        itr++;
+        *(array + iterator) = malloc(maxLength * sizeof(char));
+        if (*(array + iterator) == NULL)
+            return 1;
+        printf("Enter string: ");
+        scanf("%49s", *(array + iterator));
     }
+    return 0;
+}
+void freeArray(char **array, int numberOfStrings)
+{
+    for (int iterator = 0; iterator < numberOfStrings; iterator++)
+    {
+        free(array[iterator]);
+    }
+}
+int findMaxOccurence(char **array, int numberOfStrings, char characterToMatch, int *indexHavingMaxCharacter)
+{
+    int maxCount = -1;
+    for (int row = 0; row < numberOfStrings; row++)
+    {
+        int count = 0, column = 0;
+        while (*(*(array + row) + column) != '\0')
+        {
+            if (*(*(array + row) + column) == characterToMatch)
+                count++;
+            column++;
+        }
+        if (count > maxCount)
+        {
+            maxCount = count;
+            *indexHavingMaxCharacter = row;
+        }
+    }
+    return maxCount;
 }
 int main()
 {
-    int rows = 0;
+    int numberOfStrings = 0;
     printf("Enter number of strings: ");
-    scanf("%d", &rows);
-    char **array = malloc(sizeof(char *));
-    for (int row = 0; row < rows; row++)
+    scanf("%d", &numberOfStrings);
+    if (numberOfStrings <= 0)
     {
-        *(array + row) = malloc(50 * sizeof(char));
-        scanf("%s", *(array + row));
+        printf("Enter positive non-zero value of number of strings.\n");
+        return 0;
     }
-    char character;
-    int maxcount = 0;
-    char string[50];
+    char **array = malloc(numberOfStrings * sizeof(char *));
+    if (array == NULL)
+    {
+        printf("Memory allocation failed.\n");
+        return 0;
+    }
+    if (createArrayOfStrings(array, numberOfStrings))
+    {
+        printf("Memory allocation failed.\n");
+        return 0;
+    }
+    char characterToMatch;
+    int indexHavingMaxCharacter = 0;
     printf("Enter the character: ");
-    scanf(" %c", &character);
-    for (int row = 0; row < rows; row++)
-    {
-        int count = 0, itr = 0;
-        while (*(*(array + row) + itr) != '\0')
-        {
-            if (*(*(array + row) + itr) == character)
-                count++;
-            itr++;
-        }
-        if (count > maxcount)
-        {
-            maxcount = count;
-            copy_string(string, *(array + row));
-        }
-    }
-    printf("maxcount=%d\n", maxcount);
-    printf("%s", string);
-    for (int row = 0; row < rows; row++)
-    {
-        free(array[row]);
-    }
+    scanf(" %c", &characterToMatch);
+    int maxCountOfCharacter = findMaxOccurence(array, numberOfStrings, characterToMatch, &indexHavingMaxCharacter);
+    printf("maxcount=%d\n", maxCountOfCharacter);
+    printf("String having maximum occurence of character \'%c\' is: %s", characterToMatch, array[indexHavingMaxCharacter]);
+    freeArray(array, numberOfStrings);
     free(array);
+    array = NULL;
     return 0;
 }
